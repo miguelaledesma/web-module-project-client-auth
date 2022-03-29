3bf38a5181d3c130fc8fe028b982feb0a85a66c3
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-29 14:22:35 -0700

    started all my components in App js and set up routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,44 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
+
+const Login = () => {
+  return(<h2>Login</h2>)
+}
+
+
+
+
+const FriendsList = () => {
+  return(<h2>Friends List</h2>)
+}
+
+const AddFriend = () => {
+  return(<h2>Add Friend</h2>)
+}
+
+
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Route exact path = "/" > 
+      <Login /> 
+      </Route>
+
+      <Route exact path = "/login" > 
+      <Login /> 
+      </Route>
+
+      <Route exact path = "/friends" > 
+      <FriendsList  /> 
+      </Route>
+
+      <Route exact path = "/friends/add" > 
+      <AddFriend  /> 
+      </Route>
     </div>
+    </Router>
   );
 }
 
