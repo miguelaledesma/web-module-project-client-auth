bfbea3b9100c1b2d5d9f4b7b41f3f3759538a32a
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-29 18:19:03 -0700

    working on adding a friend, debugger is showing correct information

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,23 +1,62 @@
-import React from "react"
+import React, {useState} from "react"
+import axios from "axios"
 
+const initialFriendValues = {
+    name: "", 
+    age: "", 
+    email: ""
+}
 
+const AddFriend = () => { 
+    const [form, setForm] = useState(initialFriendValues)
+    
+    const handleChange = (evt) => {
+        const { id, value } = evt.target
+        setForm({ ...form, [id]: value })
+    }
 
-const AddFriend = () => {
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        const token = localStorage.getItem('token')
+        axios.post('http://localhost:9000/api/friends', form ,{
+            headers: {
+              authorization: token
+            }
+          })
+    .then(resp => {
+        console.log(resp)
+    })
+    .catch(err => {})
+
+    }
+
     return(
         <div>
     <h2>Add Friend</h2>
-    <form>
+    <form onSubmit = {handleSubmit}>
         <div>
         <label htmlFor = "username"> Friend Name</label>
-        <input id = "username" /> 
+        <input 
+        id = "username"
+        name = "name"
+        onChange = {handleChange}
+         /> 
         </div>
         <div>
         <label htmlFor = "age" >Age</label>
-        <input/> 
+        <input 
+        id = "age"
+        name = "age"
+        onChange = {handleChange}
+        /> 
         </div>
         <div>
         <label htmlFor = "email" >Email</label>
-        <input/> 
+        <input 
+        id = "email"
+        name = "email"
+        onChange = {handleChange}
+        /> 
         </div>
         <button>Submit</button>
     </form>
